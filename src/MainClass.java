47cb9430dc9f6629527e4a75c9af299f1e00a9bc
Prosper Ekwerike <43956851+Pekwerike@users.noreply.github.com>
2020-05-26 06:48:08 +0100

    Update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,7 @@
 public class MainClass {
     public static void main(String[] args){
         QuickSelect<String> quickSelect = new QuickSelect<>();
-        String[] names = {"Prosper", "Ekwerike", "Olamilesi", "Adeola", "Deola"};
+        String[] names = {"Prosper", "Ekwerike", "Johnson", "Adeola", "Deola"};
          System.out.println(quickSelect.select(names, 3));
     }
 }
